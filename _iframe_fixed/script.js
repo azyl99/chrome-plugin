d4e511a3c02c2b8b851f77bd40fac7c31ad88061
azyl99@win <916548981@qq.com>
2018-05-16 00:18:09 +0800

    iframe_fixed

---
+++ _iframe_fixed/script.js
@@ -0,0 +1,6 @@
+
+console.log(111)
+iframe = document.getElementById('play_ggl').children[1].children[0]
+iframe.contentDocument.getElementById('tb_2').click()
+body=document.getRootNode().children[0].children[1]
+iframe.setAttribute('style', 'position:fixed; right:0px; bottom:0px; background:#ccc;z-index:9999')
