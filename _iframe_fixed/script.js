852071a2d353c9fc662361f7f700ac30e4929c4f
azyl99@win <916548981@qq.com>
2018-05-16 10:14:14 +0800

    new version

--- _iframe_fixed/script.js
+++ _iframe_fixed/script.js
@@ -1,6 +1,16 @@
 
-console.log(111)
+console.log(2)
+
 iframe = document.getElementById('play_ggl').children[1].children[0]
-iframe.contentDocument.getElementById('tb_2').click()
+mp3src = iframe.contentDocument.getElementsByTagName('object')[0].getAttribute('data').split('=')[1]
+audio = document.createElement('audio')
+audio.setAttribute('src', mp3src)
+audio.setAttribute('autoplay', 'autoplay')
+audio.setAttribute('controls', 'controls')
+audio.setAttribute('preload', 'preload')
+audio.setAttribute('style', 'position:fixed; left:0px; bottom:0px; width:100%; z-index:9999;')
 body=document.getRootNode().children[0].children[1]
-iframe.setAttribute('style', 'position:fixed; right:0px; bottom:0px; background:#ccc;z-index:9999')
+body.appendChild(audio)
+
+
+audio.currentTime = 15;// 设置播放起点为15秒
